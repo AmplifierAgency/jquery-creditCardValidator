7bfa85ab07df47c50233c43981b910bb49fbc9e0
Marc Teichtahl <marc@teichtahl.com>
2014-12-18 22:56:29 +1100

    initial commit

---
+++ jQuery.creditCardValidator.js
@@ -0,0 +1,14 @@
+/**
+ * @file jQuery.creditCardValidator.js
+ * @author Marc Teichtahl <marc@teichtahl.com>
+ * @created 18/12/14
+ * @version 0.0.1
+ * @license MIT
+ *
+ * Copyright (c) 2014 Marc Teichtahl
+ * Licensed under the MIT license
+ *
+ * @description
+ *
+ */
+ 
